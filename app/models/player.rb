22816987697626adf71735935771377b740c274c
David Sherline <david@hypsol.com>
2017-02-12 21:34:21 -0600

    Do not destroy player if has games

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,14 @@
 class Player < ApplicationRecord
-  has_and_belongs_to_many :games, dependent: :restrict_with_exception,
-                                  inverse_of: :players
+  has_and_belongs_to_many :games, inverse_of: :players
   has_many :points, dependent: :restrict_with_exception, inverse_of: :player
 
   validates :name, presence: true, uniqueness: true
+
+  before_destroy :player_has_games, unless: 'games.empty?'
+
+  private
+
+  def player_has_games
+    errors.add(:games, :invalid, message: 'player has games', strict: true)
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Player, type: :model do
   describe 'associations' do
-    it do
-      should have_and_belong_to_many(:games).dependent(:restrict_with_exception)
-        .inverse_of(:players)
-    end
+    it { should have_and_belong_to_many(:games).inverse_of(:players) }
 
     it do
       should have_many(:points).dependent(:restrict_with_exception)
@@ -18,5 +15,26 @@ RSpec.describe Player, type: :model do
 
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
+  end
+
+  describe '#player_has_games' do
+    let(:player) { create(:player) }
+
+    context 'when the player does not have games' do
+      before(:each) { player.destroy! }
+
+      it 'should destroy the user' do
+        expect(player.destroyed?).to be(true)
+      end
+    end
+
+    context 'when the player has games' do
+      let!(:game) { create(:game, players: [player]) }
+
+      it 'should raise ActiveModel::StrictValidationFailed' do
+        expect { player.destroy! }
+          .to raise_error(ActiveModel::StrictValidationFailed)
+      end
+    end
   end
 end
